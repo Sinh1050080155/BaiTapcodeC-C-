49382583898fe7c676de6f767fc84abda51f9697
Sinh1050080155 <125429145+Sinh1050080155@users.noreply.github.com>
2023-03-02 18:53:16 +0700

    Add files via upload

---
+++ baitap3.cpp
@@ -0,0 +1,24 @@
+﻿/*
+	2.2.  Hãy cài đặt hàm đệ quy tính T(n) = n! = 1 x 2 x 3 x … x n. Trong đó T(0) = 1
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int giaithu(int n)
+{
+	if (n == 0) {
+		return 1;
+	}
+	return n*giaithu(n - 1);
+}
+int main()
+{
+	int n, giaithua;
+
+	printf("Nhap n: ");
+	scanf_s("%d", &n);
+
+	int T = giaithu(n);
+	printf("Tinh giai thua: %d\n", T);
+}
